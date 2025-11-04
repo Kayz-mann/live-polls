8abbd1a972b94150857d6c727cce3541b4999388
Kayz-mann <kayodefredrickbalogun@gmail.com>
2025-11-04 07:55:46 +0100

    Initial Commit

---
+++ LivePolls/LivePollsApp.swift
@@ -0,0 +1,32 @@
+//
+//  LivePollsApp.swift
+//  LivePolls
+//
+//  Created by Balogun Kayode on 04/11/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LivePollsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ LivePollsTests/LivePollsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LivePollsTests.swift
+//  LivePollsTests
+//
+//  Created by Balogun Kayode on 04/11/2025.
+//
+
+import Testing
+@testable import LivePolls
+
+struct LivePollsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
